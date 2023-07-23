d1efa460e090a72a388b88d10406e5b02468ecfb
Hermanda Mak <hermandamak@gmail.com>
2023-07-22 18:33:36 -0700

    add store function for lipsum start

--- popup.js
+++ popup.js
@@ -11,7 +11,7 @@ const word = ['dolor', 'sit', 'amet', 'consectetur', 'adipiscing', 'elit', 'ut',
 //.. DOM elements
 
 // user input
-const start = document.getElementById('startWithLorem')
+const checkboxLipsum = document.getElementById('startWithLorem')
 const numSentence = document.getElementById('numSentence')
 const numParagraph = document.getElementById('numParagraph')
 
@@ -25,6 +25,15 @@ const buttonTitle = document.getElementById('buttonTitle')
 const buttonParagraph = document.getElementById('buttonParagraph')
 const buttonArticle = document.getElementById('buttonArticle')
 const buttonCopy = document.getElementById('buttonCopy')
+
+//.. set defaults functions
+
+chrome.storage.local.get(["lorem-chrome-lipsum"])
+  .then((res) => {
+    console.log("start: ", res["lorem-chrome-lipsum"])
+    checkboxLipsum.checked = res["lorem-chrome-lipsum"] === "true"
+  })
+  .catch((err) => console.error(err))
 
 
 //.. helper functions
@@ -149,28 +158,44 @@ const generateArticle = (lipsum=false, n=5) => {
   return article.join('\n\n');
 }
 
+//.. checkbox/input event listeners
+
+checkboxLipsum.addEventListener("click", () => {
+  console.log(checkboxLipsum.checked)
+  chrome.storage.local.set(
+    {"lorem-chrome-lipsum": (checkboxLipsum.checked).toString()},
+    () => {
+      chrome.storage.local.get(["lorem-chrome-lipsum"])
+        .then((res) => console.log("Value is " + JSON.stringify(res)))
+      console.log("Done")
+    }
+  );
+})
+
+
+
 //.. button event listeners
 
 buttonSentence.addEventListener("click", () => {
-  container.innerText = generateSentence(start.checked);
+  container.innerText = generateSentence(checkboxLipsum.checked);
 })
 
 buttonTitle.addEventListener("click", () => {
-  container.innerText = generateTitle(start.checked);
+  container.innerText = generateTitle(checkboxLipsum.checked);
 })
 
 buttonParagraph.addEventListener("click", () => {
   if (numSentence.valueAsNumber < 1 || numSentence.valueAsNumber > 20)  
     warning('Number of sentences per paragraph is limited to 1 to 20.');
   else  
-    container.innerText = generateParagraph(start.checked, numSentence.valueAsNumber);
+    container.innerText = generateParagraph(checkboxLipsum.checked, numSentence.valueAsNumber);
 })
 
 buttonArticle.addEventListener("click", () => {
   if (numParagraph.valueAsNumber < 1 || numParagraph.valueAsNumber > 10)  
   warning('Number of paragraphs per article is limited to 1 to 10.');
   else 
-    container.innerText = generateArticle(start.checked, numParagraph.valueAsNumber);
+    container.innerText = generateArticle(checkboxLipsum.checked, numParagraph.valueAsNumber);
 })
 
 buttonCopy.addEventListener("click", copyContent)
